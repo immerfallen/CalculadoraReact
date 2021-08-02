b9d29f1055b373334c858d6a9aee26fc3543682c
Maro de Melo <marodemelo10@gmail.com>
2021-08-02 20:42:31 -0300

    concluida implementacao do front end

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import './Button.css'
+import React from "react";
+import "./Button.css";
 
-export default props => {
-    return (
-        <button className="button">{props.label}</button>
+export default (props) => {
+    
 
-    )
-}
+    //Outra forma de fazer a adicao de classes
+    /* let classes = 'button '
+    classes += props.operation ? "operation" : ""
+    classes += props.double ? "double" : ""
+    classes += props.triple ? "triple" : "" */
+
+  return (
+    <button
+    onClick={e =>  props.click && props.click(props.label)}
+      className={`button ${props.operation ? "operation" : ""} 
+        ${props.double ? "double" : ""} 
+        ${props.triple ? "triple" : ""}`}
+    >
+      {props.label}
+    </button>
+  );
+};
